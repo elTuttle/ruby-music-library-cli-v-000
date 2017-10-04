62e587fe678b416033d55e047487e3559bb85ff6
John Tuttle <thejwt@gmail.com>
2017-10-04 18:08:22 +0000

    automatically backed up by learn

--- config/environment.rb
+++ config/environment.rb
@@ -4,6 +4,14 @@ Bundler.require
 module Concerns
   module Findable
 
+    def self.find_by_name(name)
+      self.all.find {|song| song.name == name }
+    end
+
+    def self.find_or_create_by_name(name)
+      self.find_by_name(name) ? self.find_by_name(name) : self.create(name)
+    end
+    
   end
 end
 
