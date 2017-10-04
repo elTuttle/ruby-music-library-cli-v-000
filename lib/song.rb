f94a01137b1393ee39779d4cd558bddf21d59831
John Tuttle <thejwt@gmail.com>
2017-10-04 18:13:39 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -33,6 +33,11 @@ class Song
     new_song
   end
 
+  def artist_name=(name)
+    self.artist = Artist.find_or_create_by_name(name)
+    artist.add_song(self)
+  end
+  
   def artist
     @artist
   end
