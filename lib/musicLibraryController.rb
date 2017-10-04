1ad27b014552eef81b87cd6827474ab609201301
John Tuttle <thejwt@gmail.com>
2017-10-04 19:04:46 +0000

    automatically backed up by learn

--- lib/musicLibraryController.rb
+++ lib/musicLibraryController.rb
@@ -31,7 +31,7 @@ class MusicLibraryController
     temp_array = Song.all.sort { |aname, bname| aname.name <=> bname.name }
 
     temp_array.each_with_index do |val,index|
-      puts "#{index+1}. #{val.artist}"
+      puts "#{index+1}. #{val.artist.name}"
     end
 
 
